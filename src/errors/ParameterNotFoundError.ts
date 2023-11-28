3b78f2c5c2136e8685e63413ae44750b5eefb7a7
Constantine Peresypkin <pconstantine@gmail.com>
2023-11-28 15:33:16 -0500

    fix: wrong error message type
    remove some original gactions

--- src/errors/ParameterNotFoundError.ts
+++ src/errors/ParameterNotFoundError.ts
@@ -4,6 +4,6 @@ export class ParameterNotFoundError extends ServiceError {
   public constructor(
     message = "The parameter couldn't be found. Verify the name and try again."
   ) {
-    super("ParameterNotFoundError", message);
+    super("ParameterNotFound", message);
   }
 }
